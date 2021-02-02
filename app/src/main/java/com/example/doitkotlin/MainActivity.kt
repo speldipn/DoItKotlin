2244d66d4b33f19b45131d6fef1bd3fd2dfac7f3
오준영 <ojun-yeong@ojun-yeong-ui-iMac.local>
2021-02-03 06:07:57 +0900

    kotlin math

--- app/src/main/java/com/example/doitkotlin/MainActivity.kt
+++ app/src/main/java/com/example/doitkotlin/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.doitkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.math.PI
+import kotlin.math.abs
+
+//import com.example.doitkotlin.test.Person
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = "speldipn"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setup()
+    }
+
+    private fun setup() {
+        debug("${abs(-10)}")
+        debug("${PI}")
+    }
+
+    private fun debug(msg: String) {
+        Log.d(TAG, msg)
     }
 }
---
+++ app/src/main/java/com/example/doitkotlin/test/Person.kt
@@ -0,0 +1,3 @@
+package com.example.doitkotlin.test
+
+data class Person(val name: String, val age: Int, val phone:String = "01073195025")
