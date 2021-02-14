6a721440dfa59ee925b30af62d6049c2714d5ffc
Neo <speldipn@gmail.com>
2021-02-14 23:13:17 +0900

    Repeat example

--- app/src/main/java/com/example/doitkotlin/MainActivity.kt
+++ app/src/main/java/com/example/doitkotlin/MainActivity.kt
@@ -30,6 +30,26 @@ class MainActivity : AppCompatActivity() {
 
 //    private fun runRepeatExample() {
 //        runBlocking {
+//            val startTime = System.currentTimeMillis()
+//            val job = GlobalScope.launch {
+//                var nextPrintTime = startTime
+//                var i = 0
+//                while (i < 5) {
+//                    if (System.currentTimeMillis() >= nextPrintTime) {
+//                        debug("I'm sleeping ${i++}...")
+//                        nextPrintTime += 500L
+//                    }
+//                }
+//            }
+//            delay(1300L)
+//            debug("main: I'm tired of wating!")
+//            job.cancelAndJoin()
+//            debug("main: Now I can quit.")
+//        }
+//    }
+
+//    private fun runRepeatExample() {
+//        runBlocking {
 //           val job = GlobalScope.launch {
 //               try {
 //                   repeat(100) { i ->
