415839e2ec6648493518ca0cc659c92dddb79a34
Neo <speldipn@gmail.com>
2021-02-14 20:58:26 +0900

    Coroutine sequence example

--- app/src/main/java/com/example/doitkotlin/MainActivity.kt
+++ app/src/main/java/com/example/doitkotlin/MainActivity.kt
@@ -28,6 +28,31 @@ class MainActivity : AppCompatActivity() {
     private fun setup() {
     }
 
+//    private fun runSequenceExample() {
+//        val fibonacciSeq = sequence {
+//            var a = 0
+//            var b = 1
+//            yield(1)
+//
+//            while (true) {
+//                yield(a + b)
+//                val tmp = a + b
+//                a = b
+//                b = tmp
+//            }
+//        }
+//        debug("${fibonacciSeq.take(8).toList()}")
+//
+//        val seq = sequence {
+//            val start = 0
+//
+//            yield(start)
+//            yieldAll(1..5 step 2)
+//            yieldAll(generateSequence(8) { it * 3 })
+//        }
+//        debug("${seq.take(7).toList()}")
+//    }
+
 //    private fun runRepeatExample() {
 //        runBlocking {
 //            repeat(100_000) {
