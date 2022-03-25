14332afc3e31cd5e82c232f0df60d7813a3512f9
오준영 <neo@ojun-yeongs-iMac.local>
2022-03-26 01:45:37 +0900

    reset all

--- app/src/main/java/com/example/doitkotlin/Person.kt
+++
@@ -1,3 +0,0 @@
-package com.example.doitkotlin
-
-data class Person(val name: String, val age: Int)
--- app/src/main/java/com/example/doitkotlin/test/Person.kt
+++
@@ -1,3 +0,0 @@
-package com.example.doitkotlin.test
-
-data class Person(val name: String, val age: Int, val phone:String = "01073195025")
