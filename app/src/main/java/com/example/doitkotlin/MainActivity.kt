b468e3378c525e6846a3a5856406a8ad363f9401
오준영 <neo@ojun-yeongs-iMac.local>
2022-04-08 16:49:09 +0900

    init

--- app/src/main/java/com/example/doitkotlin/MainActivity.kt
+++ app/src/main/java/com/example/doitkotlin/MainActivity.kt
@@ -2,12 +2,7 @@ package com.example.doitkotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.sync.Mutex
-import kotlin.system.measureTimeMillis
 
 
 fun _println(args: Any?) = println("${MainActivity.TAG}: ${args}")
@@ -25,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     run()
   }
 
-  fun timeElapsed(task: () -> Unit) {
+  private fun timeElapsed(task: () -> Unit) {
     val before = System.nanoTime()
     task()
     val after = System.nanoTime()
@@ -33,37 +28,10 @@ class MainActivity : AppCompatActivity() {
     _println("time elapsed: $speed ms")
   }
 
-  val counterContext = newSingleThreadContext("CounterContext")
-  private var counter = 0
-  private var mutex = Mutex()
-
-  private suspend fun massiveRun(action: suspend () -> Unit) {
-    val n = 100
-    val k = 100
-    val time = measureTimeMillis {
-      val jobs = List(n) {
-        GlobalScope.launch {
-          repeat(k) { action() }
-        }
-      }
-      jobs.forEach { it.join() }
-    }
-    _println("Completed ${n * k} acitons in $time ms")
-  }
-
   private fun run() {
     timeElapsed {
       runBlocking {
-        massiveRun {
-          try {
-            mutex.lock()
-            counter++
-            mutex.unlock()
-          } catch(e: Exception) {
-            _println("execption: $e")
-          }
-        }
-        _println("Counter: ${counter}")
+        // TODO
       }
     }
   }
